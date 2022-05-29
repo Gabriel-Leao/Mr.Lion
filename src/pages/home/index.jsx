fb99149fbbb5e0ffa7cd4fa73604cb54bc1fadc1
Gabriel-Leao <gabriel.leao2507@gmail.com>
2022-05-29 10:45:48 -0300

    Newsletter section  done

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,6 +1,7 @@
 import Header from "../../Components/header/index"
 import Main from "../../Components/main"
 import Menu from "../../Components/menu"
+import Newsletter from "../../Components/newsletter"
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
       <Header />
       <Menu />
       <Main />
+      <Newsletter />
     </>
   )
 }
